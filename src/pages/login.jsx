602d9324ff1d24060937adef32e9e3da14bd8b77
Erick Vinicius <erick.viniciusfff@gmail.com>
2026-03-23 04:23:52 -0300

    Atualização sobre o campo login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -94,7 +94,7 @@ function Login() {
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                     className="w-full border border-slate-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition"
-                                    placeholder="admin@hotelstayo.com"
+                                    placeholder="exemplo@exemplo.com"
                                     required
                                 />
                             </div>
